e160e6bda8137cca4fd6448be522dd461084234e
mayank metha <mayankmethad@gmail.com>
2026-09-14 23:13:28 +0530

    Update Android SDK

---
+++ app/src/main/java/MainActivity.kt
@@ -0,0 +1,85 @@
+@file:Suppress("PackageName")
+package com.BalancingEquations
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.RenderProcessGoneDetail
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val webView: WebView = findViewById(R.id.appWebView)
+
+        webView.apply {
+            webChromeClient = WebChromeClient()
+            webViewClient = @SuppressLint("MissingOnRenderProcessGone")
+            object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(
+                    view: WebView,
+                    request: WebResourceRequest
+                ): Boolean {
+                    val url = request.url
+                    val host = url.host
+
+                    return if (host != null && host.endsWith("mayankmetha.github.io")) {
+                        false
+                    } else {
+                        view.context.startActivity(Intent(Intent.ACTION_VIEW, url))
+                        true
+                    }
+                }
+
+                override fun onRenderProcessGone(
+                    view: WebView?,
+                    detail: RenderProcessGoneDetail?
+                ): Boolean {
+                    if (view != null) {
+                        val parent = view.parent as? ViewGroup
+                        parent?.removeView(view)
+                        view.destroy()
+                    }
+                    // Return true to signal that the app has handled the render process crash
+                    return true
+                }
+            }
+
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+
+            loadUrl("https://mayankmetha.github.io/BalancingEquations")
+        }
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
+    }
+}
