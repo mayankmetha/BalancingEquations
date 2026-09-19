2d270a2231c3d7a4d610c430fbcf5ed7a29485e4
mayankmethad <mayankmethad@gmail.com>
2026-09-20 02:11:40 +0530

    Update Android SDK

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -25,7 +25,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+            )
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
